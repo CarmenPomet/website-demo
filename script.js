02ea2d11026e02e2769b0acfb14a19c4bd88a315
Carmen Pomet <carmenpomet@gmail.com>
2026-04-19 18:32:04 +0200

    adding typography changes

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const colors = [
-    "#FF69B4", // Hot Pink
-    "#9BE69F", //Green
-    "#FF7F00", // Orange (Healing)
-    "#00CC00", // Green (Nature)
-    "#FF00FE", // Lila
-    "#4bbaab",
-    "#5383c0",
+    "#f8b4d6", // Hot Pink
+    "#ace5ae", //Green
+    "#f5d2af", // Orange (Healing)
+    "#516c51", // Green (Nature)
+    "#d295d2", // Lila
+    "#84b2ac",
+    "#8fa1b8",
     "#87CEEB", // Light Blue (for Transgender individuals)
     "#5BCEFA", // Light Blue (traditional color for baby boys)
     "#F5A9B8", // Light Pink (traditional color for baby girls)
     "#00BFFF", // Turquoise (Magic/Art)
     "#0000FF", // Indigo/Blue (Serenity)
-    "#8B00FF",  // Violet (Spirit)
+    "#cba7e8",  // Violet (Spirit)
     "#000000"  // Black (for People of Color & those lost to AIDS)
 ];
     
